1fec8700d7e93836d61d4f4f7dc4ebd20f47d9f8
Imomali <imomaliazizov70@gmail.com>
2022-08-30 23:05:42 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^0)9hi5^m+os6za=)gw-67!oo83q8uzfz6uypz+k_dcdub&gqa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com','localhost','127.0.0.1']
-
+    
 
 # Application definition
 
